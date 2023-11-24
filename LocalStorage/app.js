6e0a734b82f89768d1e789b90fa0b8f68915f399
Nguyễn Danh Phương <nguyendanhphuong0305@gmail.com>
2023-11-24 19:21:14 +0700

    update day7

---
+++ LocalStorage/app.js
@@ -0,0 +1,9 @@
+let box = document.getElementById("box");
+
+function changeColor() {
+  if (box.style.backgroundColor == "red") {
+    box.style.backgroundColor = "black";
+  } else {
+    box.style.backgroundColor = "red";
+  }
+}
