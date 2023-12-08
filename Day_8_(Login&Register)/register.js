a537065bed863fd8c11e5a3ebd98e914d057323b
Nguyễn Danh Phương <nguyendanhphuong0305@gmail.com>
2023-12-08 20:45:16 +0700

    update day8 next (register and login final)

--- Day_8_(Login&Register)/register.js
+++ Day_8_(Login&Register)/register.js
@@ -1,16 +1,38 @@
 let username = document.getElementById("username");
+username.value = "sjdfksjnfdkjs";
+
+result_text.value = input_text.value;
+
 let password = document.getElementById("password");
 let register_btn = document.getElementById("register");
 
+let array_key_localStorage = Object.keys(localStorage); // get All Key from localStorage
+
+function checkExistedUser(username) {
+  for (let i = 0; i < array_key_localStorage.length; i++) {
+    if (username == array_key_localStorage[i]) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 register_btn.addEventListener("click", function () {
   let checkValidateInput = is_Input_Fill(username.value, password.value);
+  let checkUser = checkExistedUser(username.value);
 
   if (checkValidateInput == true) {
-    localStorage.setItem(username.value, password.value);
-    username.value = "";
-    password.value = "";
+    if (checkUser == true) {
+      alert("Tên khoàn này đã tồn tại, vui lòng thử tên đăng nhập khác");
+    } else {
+      localStorage.setItem(username.value, password.value);
+      username.value = "";
+      password.value = "";
+      alert("Đăng ký tài khoản thành công");
 
-    location.href = "./login.html";
+      location.href = "./login.html";
+    }
   }
 });
 
@@ -28,5 +50,9 @@ function is_Input_Fill(username, password) {
 
   return true;
 }
+
+let text = "nsjkdfnskdjfn";
+let newText = text.toUpperCase();
+console.log(newText);
 
 // console.log(t);
